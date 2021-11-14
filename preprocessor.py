f4f8f2384f3f082267fa7f3811d08d49a3dd9c4c
Fynn <fynn.van-westen@gmx.de>
2021-11-15 00:13:52 +0100

    added test case

--- preprocessor.py
+++ preprocessor.py
@@ -34,3 +34,45 @@ class Preprocessor:
         if self.punctuation:
             txt = ''.join(c for c in txt if c not in string.punctuation)
         return txt
+
+    def load_test_cases(self):
+        text1 = """
+        Nadal was named Sportsman of the Year after a remarkable season in which he won the French Open, Wimbledon and US Open trophies while Spain's World Cup side were named Team of the Year at a glittering ceremony attended by celebrities and sporting greats.
+        "It's an honour, in my name, that of my colleagues and the Spanish football federation, to receive this award from the world of sport," Spain coach Vicente del Bosque said in a live feed to the awards ceremony from the team's training base outside Madrid.
+        American Alpine skier Lindsay Vonn, who won the Olympic downhill gold at the Vancouver Games last February just days after suffering a shin injury, was voted Sportswoman of the Year.
+        Spain, the USA and Germany were the big winners with each country bagging two awards.
+        
+        There was a rapturous welcome for French football great Zinedine Zidane, who received the Lifetime Achievement Award in recognition of his remarkable career.
+        The Spirit of Sport Award was presented to the European Ryder Cup Team to mark their emotional win over the United States in a match "which demonstrated great sportsmanship, golf played at the highest level and enormous and passionate galleries", a statement from the organisers said.
+        The Laureus World Sports Awards, which recognise sporting achievement, are the premier honours on the international sporting calendar.
+        Double Oscar winner Kevin Spacey hosted the event attended by celebrities from the world of sports and entertainment.
+        The award winners were: 
+        Laureus World Sportsman of the Year: Rafael Nadal (Spain) 
+        Laureus World Sportswoman of the Year: Lindsey Vonn (US) 
+        Laureus World Team of the Year: Spain World Cup 
+        Team Laureus World Breakthrough of the Year: Martin Kaymer (Germany) 
+        Laureus World Comeback of the Year: Valentino Rossi (Italy) 
+        Laureus World Sportsperson of the Year with a Disability: Verena Bentele (Germany) 
+        Laureus World Action Sportsperson of the Year: Kelly Slater (US)
+        """
+        self.corpus.append(text1)
+        text2 = """
+        Nadal was named Spodrtsman of the Year after a remadrkable season in which he won the French Open, Wimbledon and US Open trophies while Spain's World Cup side were named Team of the Year at a glittering ceremony attended by celebrities and sporting greats.
+        "It's an honour, ddin my nadme, that of my codlleagues and the dSpanish football dfederation, to receive this award from the world of sport," Spain coach Vicente del Bosque said in a live feed to the awards ceremony from the team's training base outside Madrid.
+        American Alpine skier Lindsay Vonnd, who won the Olympic downhill gold at the Vancouver Games last February just days after suffering a shin injury, was voted Sportswoman of the Year.
+        Spain, the USA and Germany were the big dwinners with each country bagging two awards.
+        dd
+        There was a rapturous welcome for French football great Zinedine Zidane, who received the Lifetime Achievement Award in recognition of his remarkable career.
+        The Spirit of Sport Award was presendted to the European Ryder Cup Team to dmark their emotional win over the United States in a match "which demonstrated great sportsmanship, golf played at the highest level and enormous and passionate galleries", a statement from the organisers said.
+        The Laureus World Sports Awaasdrds, which recogdnise sporting achievement, are the premier honours on the international sporting calendar.
+        Double Oscar winner Kevind Spacey hosted the event attended by celebrities from the world of sports and entertainment.
+        The award winners were: 
+        Laureus World Sportsman of the Year: Rafael Nadal (Spain) 
+        Laureus World Sportdswoman of the Year: Lindsedy Vonn (US) 
+        Laureusd Wordddld Team of the Year: Spain World Cup 
+        Team Laureus Wdorld Breakthrough of the Yeadr: Martin Kaymer (Germany) 
+        Laureusd World Comeback of the Year: Valentino Rossi (Italy) 
+        Laureus World Sportdspersodn of the Year with a Disability: Verena Bentele (Germany) 
+        Laureus World Actiond Sportsperson of the Year: dKelly Slater (US)
+        """
+        self.corpus.append(text2)
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import argparse
 from preprocessor import Preprocessor
-from algo_lib import Shingling, CompareSets, MinHashing, CompareSignatures
+from algo_lib import Shingling, CompareSets, MinHashing, CompareSignatures, LSH
 
 parser = argparse.ArgumentParser(description='Find similar documents.')
 parser.add_argument('-dataset-dir', default='dataset/sportsArticles/raw_data', help='path to a data directory')
@@ -8,6 +8,7 @@ parser.add_argument('-n-documents', default=10, type=int, help='number of docume
 parser.add_argument('-k-shingles', default=10, type=int, help='construct shingles of character length k')
 parser.add_argument('-n-signature', default=500, type=int, help='build a minhash signature of length n')
 parser.add_argument('-sim-threshold', default=0.8, type=float, help='similarity threshold for retrieving documents')
+parser.add_argument('-testing', default=True, type=bool, help='use test case')
 
 args = parser.parse_args()
 
@@ -18,7 +19,10 @@ preprocessor = Preprocessor(TEXT_DIR = args.dataset_dir, \
     punctuation=True)
 
 print("Loading documents...")
-preprocessor.load_texts_from_dir()
+if args.testing:
+    preprocessor.load_test_cases()
+else:
+    preprocessor.load_texts_from_dir()
 documents = preprocessor.corpus
 print("Documents loaded!")
 
@@ -27,19 +31,28 @@ print("Creating characteristic matrix...")
 char_matrix = shingling.create_characteristic_matrix(documents)
 print("Characteristic matrix created!")
 
+"""
 min_hashing = MinHashing(char_matrix, args.n_signature)
 print("Creating signature matrix...")
 signature_matrix, signature_dataframe = min_hashing.create_sig_matrix()
 print("Signature matrix created!")
 
-print(signature_dataframe.head())
-print(signature_dataframe[:][0])
 
 setComparer = CompareSets()
 signatureComperer = CompareSignatures()
 # create loop to compare multiple docs against each other 
-for doc_id in range(10): # compare eachdoc against thefirst one
+if args.testing:
+    compare_against_first_x = 2
+else:
+    compare_against_first_x = args.n_documents
+for doc_id in range(compare_against_first_x): # compare eachdoc against thefirst one
     jaccard_similarity = setComparer.jaccard_similarity(char_matrix[:][0], char_matrix[:][doc_id])
     estimated_jaccard_similarity = signatureComperer.estimate_jaccard_similarity(signature_dataframe[:][0], \
                                                                                  signature_dataframe[:][doc_id])
-    print("Exact similarity is: {}, estimates similarity is:{} \n Differing by {}".format(jaccard_similarity, estimated_jaccard_similarity, (abs(jaccard_similarity-estimated_jaccard_similarity))))
+    print("Exact similarity is: {}, estimates similarity is:{} \nDiffering by {}\n".format(jaccard_similarity, estimated_jaccard_similarity, (abs(jaccard_similarity-estimated_jaccard_similarity))))
+"""
+
+print("Running LSH...")
+lsh = LSH(char_matrix,args.sim_threshold, 2)
+found = lsh.return_docs()
+print(found)
